b6aeed10626c8f6be30adca37d933c70ad4fc2a1
GavAng <103150153+GavAng@users.noreply.github.com>
2023-05-05 17:15:52 +0100

    Add files via upload

--- webScraper.py
+++ webScraper.py
@@ -1,18 +1,17 @@
 #modules
+import re
 import requests
 from bs4 import BeautifulSoup
 
 
 
+def getTableData(url):
 
+    headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"}
+    page = requests.get(url, headers=headers)
 
-def getTableData(URL):
-
-    page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
-
     return soup.find("tbody")
-
 
 
 
@@ -24,15 +23,8 @@ def getNames():
         "td", string = lambda text : not str(text)[0].isdigit()
     )
 
-
     #gets just the strings of names
-    names = [
-        nameElement.text for nameElement in nameElements[:-2]
-    ]
-    
-    return names
-
-
+    return [nameElement.text for nameElement in nameElements[:-2]]
 
 
 
@@ -41,14 +33,7 @@ def getLastNames():
     nameTable = getTableData("https://www.thoughtco.com/most-common-us-surnames-1422656")
     nameElements = nameTable.find_all("td")
 
-    lastNames = [
-        nameElement.text for nameElement in nameElements[1::4]
-    ]
-
-
-    return lastNames
-
-
+    return [nameElement.text for nameElement in nameElements[1::4]]
 
 
 
@@ -56,53 +41,36 @@ def getJobs():
 
     jobTable = getTableData("https://www.careeronestop.org/Toolkit/Careers/careers-largest-employment.aspx?pagesize=50&currentpage=1&nodata=")
     #get all the table data
-    jobElements = jobTable.find_all("td")
+    jobData = jobTable.find_all("td")
 
-
-    #gets just the singular job titles from the list
-    jobTitles = list({
-        #return the job title without the s at the end
-        jobTitle[:-1]
-        if jobTitle[-3:-1] != "ie" 
-        #if job title ends in "ies" return it with a "y" instead
-        else jobTitle[:-3] + "y"
-
-        #for every 5th element in the last starting at element 2
-        for jobElement in jobElements[1::5] 
-        if (jobTitle := jobElement.text.split(", ", 1)[0].split(" and ", 1)[0].split(" of ", 1)[0])[-1] == "s"
+    # Only accept job titles (every 5th cell in the table) where the title (cut down) ends in an 's'
+    return list({jobTitle[:-1] 
+            if jobTitle[-3:-1] != "ie" 
+            else jobTitle[:-3] + "y"
+            for jobElement in jobData[1::5]
+            if (jobTitle := re.split(",| and | of ", jobElement.text.strip())[0])[-1] == "s"
     })
-
-
-    return jobTitles
-
-
 
 
 
 def getCountries():
 
     countryTable = getTableData("https://www.worldometers.info/geography/alphabetical-list-of-countries/")
-    #get all the table data that is not a numerical figure
     countryElements = countryTable.find_all("td")
 
-
-    #gets just the current country names from the list where the population is greater than 3,000,000
+    #gets just the current country names where the population is greater than 3,000,000
     countryNames = [
         countryElements[index + 1].text.split(" (", 1)[0]
         for index in range(0,len(countryElements),5)
-        if int(countryElements[index + 2].text.replace(",","")) > 3000000
+        if int(countryElements[index + 2].text.replace(",","")) > 10_000_000
     ]
-    
-    
     return countryNames
-
-
 
 
 
 if __name__ == "__main__":
 
-    print(getNames())
-    print(getLastNames())
-    print(getJobs())
+    #print(getNames())
+    #print(getLastNames())
+    #print(getJobs())
     print(getCountries())
